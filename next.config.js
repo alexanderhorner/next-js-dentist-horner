626f96f29305f1e201da894bb786b43f78ad322f
Alexander Horner <mail@alexanderhorner.com>
2023-04-17 11:51:22 +0200

    Update next.config.js to send dentisthorner domain to En locale

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   i18n: {
     locales: ['en', 'de'],
     defaultLocale: 'de',
+    
+    domains: [
+      {
+        domain: 'www.dentisthorner.com',
+        defaultLocale: 'en',
+      }
+    ],
   },
 }
 
